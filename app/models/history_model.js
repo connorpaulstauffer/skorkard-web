4ed1b56e91d64236730a1c7bc2159ca39d958d9c
Connor Stauffer <cstauffer@buildzoom.com>
2016-05-17 15:41:00 -0700

    use ramda inc and dec methods

--- app/models/history_model.js
+++ app/models/history_model.js
@@ -1,12 +1,13 @@
 /*jshint esversion: 6 */
 import { createModel } from './../helpers/model';
 import { just } from 'most';
+import { inc, dec } from 'ramda';
 
 const historyMods = {
 	ADD_OCCURRENCE: (history, { date, habitId }) =>
-	 	evolve({ [date]: n => n + 1 }, history),
+	 	evolve({ [date]: inc }, history),
 	REMOVE_OCCURRENCE: (history, { date, habitId }) =>
-	 	evolve({ [date]: n => n - 1 }, history)
+	 	evolve({ [date]: dec }, history)
 };
 
 const HistoryModel = () => {
